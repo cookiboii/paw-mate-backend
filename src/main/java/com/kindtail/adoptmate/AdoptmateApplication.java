5e025ddf80dd5cc778090a9dc256fcc35046e43d
naseokhoo <nahoo1202@naver.com>
2025-07-02 11:36:28 +0900

    first commit

---
+++ src/main/java/com/kindtail/adoptmate/AdoptmateApplication.java
@@ -0,0 +1,13 @@
+package com.kindtail.adoptmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptmateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdoptmateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kindtail/adoptmate/AdoptmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kindtail.adoptmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptmateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
