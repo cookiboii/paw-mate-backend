b97a068f5683a4d6c8ab6bb2e185d5f3a352cbc8
nahoo <nahoo1202@naver.com>
2026-08-08 00:41:30 +0900

    레디스 config쪽수정

--- src/main/java/com/kindtail/adoptmate/config/RedisConfig.java
+++ src/main/java/com/kindtail/adoptmate/config/RedisConfig.java
@@ -34,6 +34,7 @@ public class RedisConfig {
         redisStandaloneConfiguration.setPassword(password);
         LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
         lettuceConnectionFactory.setUseSsl(true);
+        lettuceConnectionFactory.setVerifyPeer(false);
     // Upstash 도메인 검증 우회
         return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
