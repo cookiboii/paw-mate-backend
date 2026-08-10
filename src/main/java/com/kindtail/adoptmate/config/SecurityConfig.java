2c4cb484790ebdf9239661725e09a84637ea0388
nahoo <nahoo1202@naver.com>
2026-08-10 16:08:07 +0900

    시큐티리쪽 수정

--- src/main/java/com/kindtail/adoptmate/config/SecurityConfig.java
+++ src/main/java/com/kindtail/adoptmate/config/SecurityConfig.java
@@ -74,7 +74,8 @@ public class SecurityConfig {
                     "/h2-console/**",
                     "/v3/api-docs/**",
                     "/swagger-ui/**",
-                    "/uploads/**"
+                    "/uploads/**",
+                            "/adoptions/animals/**"
             ).permitAll()
             .requestMatchers(HttpMethod.GET, "/animals/**", "/post/**", "/comment/**").permitAll()
             .anyRequest().authenticated();
